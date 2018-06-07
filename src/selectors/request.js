cd63a189d5999597d1942bf2838a82c51e63b04a
Wojciech Maj <kontakt@wojtekmaj.pl>
2018-06-07 14:15:06 +0200

    Add error on missing Redux state

--- src/selectors/request.js
+++ src/selectors/request.js
@@ -2,9 +2,16 @@ export const makeGetRequest = stateKey => (state, requestKey) => state[stateKey]
 
 export const makeGetRows = stateKey => (state, requestKey) => {
   const statePiece = state[stateKey];
+
+  if (!statePiece) {
+    throw new Error('Missing Redux state.');
+  }
+
   const recordIds = statePiece.getIn(['requests', ...[].concat(requestKey), 'data']);
+
   if (!recordIds) {
     return recordIds;
   }
+
   return recordIds.map(recordId => statePiece.getIn(['records', recordId]));
 };
